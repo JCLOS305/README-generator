6c098faaeb72df2600386dc0a37c8933942d7d5e
jclos305 <jclos305@hotmail.com>
2022-02-18 09:56:56 -0500

    readmeGenerator.js updated with appending information

---
+++ readmeGenerator.js
@@ -0,0 +1,64 @@
+function readmeGenerator(response) {
+    var content = `# ${response.title}
+    ##### [${response.userName}](${response.GitHub}) [Email Me](${response.email}) 
+    #### ReadMeGenerator© 2022 All Rights Reserved.  ${response.license}
+    
+    ## Table of Contents
+    * [Repository Description](#Repository-Description)
+    * [User Story](#User-Story)
+    * [Installation Instructions](#Install)
+    * [Usage Information](#Usage)
+    * [Screenshots](#Screen-Shot)
+    * [License](#License) 
+
+    
+    # Repository Description
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    ${response.description}
+    
+
+    
+    # User Story
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    AS a developer
+    I WANT a README generator
+    SO THAT I can quickly create a professional README for a new project
+
+    
+    # Instalation Instructions
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    ${response.install}
+
+    # Usage Information
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    ${response.usage}
+
+  
+    # Contribution Guidelines
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    ${response.contribute}
+    
+    
+    # Test Instructions
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    ${response.test}
+    
+    
+    # Screenshots 
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    #### Demo  [screenshot.js](${response.screenshotjs})
+    
+    
+
+    
+   
+    # License
+    ${response.license}
+    
+   
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+    return content;
+};
+
+module.exports = readmeGenerator;
